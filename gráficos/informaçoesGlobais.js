c94faa70d335cf6200ea896a67a819bca9dcb0e8
marcelinhapiloni <126159389+marcelinhapiloni@users.noreply.github.com>
2024-10-10 11:45:05 +0000

    10/10

--- gráficos/informaçoesGlobais.js
+++ gráficos/informaçoesGlobais.js
@@ -4,5 +4,7 @@ async function visualizarInformaçoesGlobais() {
     const dados = await res.json()
     console.log(dados);
     const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = 'Você sabia que o mundo tem ${dados.total_pessoa_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas'
 }
 visualizarInformaçoesGlobais()
