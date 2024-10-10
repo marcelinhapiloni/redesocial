2d5b805439ef042a182706977494516dd41deadf
marcelinhapiloni <126159389+marcelinhapiloni@users.noreply.github.com>
2024-10-10 11:16:53 +0000

    10/10

--- gráficos/informaçoesGlobais.js
+++ gráficos/informaçoesGlobais.js
@@ -1,4 +1,6 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 async function visualizarInformaçoesGlobais() {
-
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
 }
