6e01dbbc1a8c97a26a0b92dd4e0e82976188ee30
marcelinhapiloni <126159389+marcelinhapiloni@users.noreply.github.com>
2024-10-17 08:40:55 -0300

    17/10

--- gráficos/informaçoesGlobais.js
+++ gráficos/informaçoesGlobais.js
@@ -8,7 +8,7 @@ async function visualizarInformaçoesGlobais() {
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = 'Você sabia que o mundo tem ${dados.total_pessoa_mundo}de pessoas e que aproximadamente ${dados.total_pessoas_conectadas}estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas'
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     console.log(paragrafo)
     const container = document.getElementById ('graficos-container')
     container.appendChild(paragrafo)
